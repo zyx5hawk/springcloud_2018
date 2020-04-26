18b9afb96cc13a49298eed7922c39e5441c1b6b2
yingxin zhao <zhaoyingxin@CZ1CG1.PRI>
2020-04-26 13:45:16 +0800

    second

--- microservicecloud/microservicecloud_entities/src/main/java/com/zyx/microservicecloud/entities/Dept.java
+++ microservicecloud/microservicecloud_entities/src/main/java/com/zyx/microservicecloud/entities/Dept.java
@@ -4,16 +4,42 @@ import java.io.Serializable;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
-@Data
 public class Dept implements Serializable {
 	private Long deptno;
 	private String dname;
 	private String db_source;
 
+	public Dept() {
+
+	}
+
 	public Dept(String dname) {
 		super();
 		this.dname = dname;
+	}
+
+	public Long getDeptno() {
+		return deptno;
+	}
+
+	public void setDeptno(Long deptno) {
+		this.deptno = deptno;
+	}
+
+	public String getDname() {
+		return dname;
+	}
+
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+
+	public String getDb_source() {
+		return db_source;
+	}
+
+	public void setDb_source(String db_source) {
+		this.db_source = db_source;
 	}
 
 }
